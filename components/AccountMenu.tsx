90261affa91d0abbfafc7cef735f5fec43a6002f
Surya prakash <2010039001@klh.edu.in>
2023-04-25 01:14:50 +0530

    Netflix-clone-Final

--- components/AccountMenu.tsx
+++ components/AccountMenu.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import {signOut} from 'next-auth/react';
 import useCurrentUser from "@/hooks/useCurrentUser";
-import Image from 'next/image';
 
 interface AccountMenuProps{
     visible: boolean;
@@ -12,12 +11,12 @@ const AccountMenu:React.FC<AccountMenuProps>  = ({
     if(!visible){
         return null;
     }
-    const { data: currentUser } = useCurrentUser();
+    const {data: currentUser} = useCurrentUser();
     return (
     <div className="bg-black w-56 absolute top-20  right-10 py-5 flex-col border-2 border-gray-800 flex">
         <div className="flex flex-col gap-3">
             <div className="px-3 group/item flex flex-row gap-3 items-center w-full">
-                <Image className="rounded-md w-8" width={20} height={20} src="/images/default-blue.png"  alt="" />
+                <img className="rounded-md w-8" src="/images/default-blue.png" alt="" />
                 <p className="text-white text-sm group-hover/item:underline">
                     {currentUser?.name}
                 </p>
